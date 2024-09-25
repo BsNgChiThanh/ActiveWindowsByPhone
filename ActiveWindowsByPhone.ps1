020985e09a0fae6c516c70a2e85e1eefa70ae78c
Nguyen Chi Thanh <82578024+BsNgChiThanh@users.noreply.github.com>
2024-09-25 14:00:13 +0700

    Update ActiveWindowsByPhone.ps1

--- ActiveWindowsByPhone.ps1
+++ ActiveWindowsByPhone.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/Kich-hoat-Windows/refs/heads/IMP/ActiveWindowsByPhone.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/BsNgChiThanh/ActiveWindowsByPhone/IMP/ActiveWindowsByPhone.cmd'
 
 $rand = Get-Random -Maximum 1000
 $FilePath = "$env:TEMP\ActiveWindowsByPhone_$rand.cmd"
